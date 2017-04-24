464f866f8dafabd69f433aa63573153b0adda989
Mitchell Oleson <moleson21@gmail.com>
2017-04-24 14:18:31 -0700

    Updates to GUI and bluetooth adding

--- GUI/main.c
+++ GUI/main.c
@@ -1,18 +1,43 @@
 #include <windows.h>
+#include <stdio.h>
 #include "resource.h"
 
-/* Window Name. */
+/* Definitions. */
+#define MAX_DEVICES     25
+#define MAX_NAME_LEN    50
+#define BLUE_ADDR_LEN   17
+
+/* Structures. */
+struct blue_config
+{
+  char BD_NAME[MAX_NAME_LEN];
+  char BD_ADDR[BLUE_ADDR_LEN];
+  BOOL FULL;
+};
+
+/* Window constants */
 const char winClassName[] = "controlPanel";
+struct blue_config *bluetooth_configs[MAX_DEVICES];
+int cur_device = 0;
+int update_device = -1;
+int connected_device = -1;
 
 /* Function Prototypes. */
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
   LPSTR lpCmdLine, int nCmdShow);
 LRESULT CALLBACK WndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+void DoFileCreate (HWND hwnd);
 void DoFileOpen (HWND hwnd);
 void DoFileSave (HWND hwnd);
 void DoHelpAbout (HWND hwnd);
+BOOL SaveConfig (HWND hEdit, LPCTSTR pszFileName);
+BOOL LoadConfig (HWND hEdit, LPCTSTR pszFileName);
+BOOL CALLBACK AboutDlgProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK ConfigEditDlgProc (HWND hwnd, UINT  msg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK ConfigCreateDlgProc (HWND hwnd, UINT  msg, WPARAM wParam, LPARAM lParam);
 
-int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
+int WINAPI
+WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
   LPSTR lpCmdLine, int nCmdShow)
 {
   /* Setup Window Functionals. */
@@ -20,7 +45,18 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
   HWND hwnd;
   MSG msg;
 
-  /* Assign defaults. */
+  /* Create default bluetooth device. */
+  struct blue_config *blue;
+  blue = (struct blue_config*) GlobalAlloc (GPTR, sizeof (struct blue_config));
+  memcpy(blue->BD_NAME, "DEFAULT", MAX_NAME_LEN);
+  memcpy(blue->BD_ADDR, "B8:27:EB:A1:D8:3F", BLUE_ADDR_LEN);
+  blue->FULL = TRUE;
+
+  bluetooth_configs[cur_device] = blue;
+  cur_device++;
+
+
+  /* Assign window defaults. */
   wc.cbSize = sizeof(WNDCLASSEX);
   wc.style = 0;
   wc.lpfnWndProc = WndProc;
@@ -47,7 +83,7 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
   hwnd = CreateWindowEx(
     0,
     winClassName,
-    "#NoSleep: Control Window",
+    "#NoSleep: Control",
     WS_OVERLAPPEDWINDOW,
     CW_USEDEFAULT, CW_USEDEFAULT, 640, 480,
     NULL, NULL, hInstance, NULL);
@@ -74,23 +110,29 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
   return msg.wParam;
 }
 
-LRESULT CALLBACK WndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK 
+WndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
   switch (msg)
   {
     case WM_CREATE:
     {
       HFONT hfDefault;
-      HWND hEdit;
+      hfDefault = (HFONT) GetStockObject (DEFAULT_GUI_FONT);
 
-      hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, "EDIT", "", 
+      HWND hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, "EDIT", "", 
         WS_CHILD | WS_VISIBLE | WS_VSCROLL | WS_HSCROLL | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL, 
         0, 0, 100, 100, hwnd, (HMENU) ID_NOSLEEP_MAIN, GetModuleHandle (NULL), NULL);
       if (hEdit == NULL)
         MessageBox (hwnd, "Could not create edit box.", "Error", MB_OK | MB_ICONERROR);
-
-      hfDefault = (HFONT) GetStockObject (DEFAULT_GUI_FONT);
       SendMessage (hEdit, WM_SETFONT, (WPARAM) hfDefault, MAKELPARAM (FALSE, 0));
+
+      HWND but1 = CreateWindow("BUTTON", "OK",
+        WS_CHILD | WS_VISIBLE | WS_TABSTOP | BS_DEFPUSHBUTTON,
+        100, 100, 100, 100, hwnd, NULL, GetModuleHandle (NULL), NULL);
+      if (but1 == NULL)
+        MessageBox (hwnd, "Could not create button.", "Error", MB_OK | MB_ICONERROR);
+      SendMessage (but1, WM_SETFONT, (WPARAM) hfDefault, MAKELPARAM (FALSE, 0));
     }
     break;
     case WM_CLOSE:
@@ -105,14 +147,15 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
         case ID_FILE_EXIT:
           PostMessage (hwnd, WM_CLOSE, 0, 0);
         break;
-        case ID_FILE_NEW:
-          SetDlgItemText (hwnd, ID_NOSLEEP_MAIN, "");
+        case ID_CONFIG_EDIT:
+          DialogBox (GetModuleHandle (NULL),
+            MAKEINTRESOURCE (ID_NOSLEEP_LIST), hwnd, ConfigEditDlgProc);
         break;
-        case ID_FILE_OPEN:
-          DoFileOpen (hwnd);
-        break;
-        case ID_FILE_SAVEAS:
+        case ID_CONFIG_SAVEAS:
           DoFileSave (hwnd);
+        break;
+        case ID_CONFIG_LOAD:
+          DoFileOpen (hwnd);
         break;
         case ID_HELP_ABOUT:
           DoHelpAbout (hwnd);
@@ -122,55 +165,418 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     default:
       return DefWindowProc (hwnd, msg, wParam, lParam);
   }
-
   return 0;
 }
 
-void DoFileOpen (HWND hwnd)
+BOOL CALLBACK
+ConfigCreateDlgProc (HWND hwnd, UINT  msg, WPARAM wParam, LPARAM lParam)
 {
+  switch (msg)
+  {
+    case WM_INITDIALOG:
+    {
+      if (update_device == -1)
+      {
+        SendDlgItemMessage(hwnd, ID_CREATE_FULL, BM_SETCHECK, 1, 0);
+        SendDlgItemMessage(hwnd, ID_CREATE_SINGLE, BM_SETCHECK, 0, 0);
+      } else
+      {
+        struct blue_config *b = bluetooth_configs[update_device];
+        SetWindowText (hwnd, "Update Cube...");
+        SetDlgItemText (hwnd, ID_CREATE_ADD, "Update");
+        SetDlgItemText (hwnd, ID_CREATE_NAME, (LPCTSTR) b->BD_NAME);
+        SetDlgItemText (hwnd, ID_CREATE_ADDR, (LPCTSTR) b->BD_ADDR);
+        if (b->FULL)
+        {
+          SendDlgItemMessage(hwnd, ID_CREATE_FULL, BM_SETCHECK, 1, 0);
+          SendDlgItemMessage(hwnd, ID_CREATE_SINGLE, BM_SETCHECK, 0, 0);
+        } else
+        {
+          SendDlgItemMessage(hwnd, ID_CREATE_SINGLE, BM_SETCHECK, 1, 0);
+          SendDlgItemMessage(hwnd, ID_CREATE_FULL, BM_SETCHECK, 0, 0);
+        }
+      }
+    }
+    break;
+    case WM_COMMAND:
+      switch (LOWORD (wParam))
+      {
+        case ID_CREATE_ADD:
+        {
+          int len_name = GetWindowTextLength (GetDlgItem (hwnd, ID_CREATE_NAME));
+          int len_addr = GetWindowTextLength (GetDlgItem (hwnd, ID_CREATE_ADDR));
+          int full = SendDlgItemMessage(hwnd, ID_CREATE_FULL, BM_GETCHECK, 0, 0);
+          if (0 < len_name && 0 < len_addr)
+          {
+            if (MAX_NAME_LEN < len_name)
+              MessageBox (hwnd, "Name too long!", "Error", MB_OK);
+            else if (len_addr != BLUE_ADDR_LEN)
+              MessageBox (hwnd, "Bluetooth address incorrect length!", "Error", MB_OK);
+            else
+            {
+              struct blue_config *blue;
+              if (update_device == -1)
+                blue = (struct blue_config*) GlobalAlloc (GPTR, sizeof (struct blue_config));
+              else
+                blue = bluetooth_configs[update_device];
+
+              GetDlgItemText (hwnd, ID_CREATE_NAME, (char*) blue->BD_NAME, len_name + 1);
+              GetDlgItemText (hwnd, ID_CREATE_ADDR, (char*) blue->BD_ADDR, len_addr + 1);
+              if (full == 1)
+                blue->FULL = TRUE;
+              else
+                blue->FULL = FALSE;
+
+              HWND parent = GetParent (hwnd);
+              SendDlgItemMessage (parent, ID_CONFIG_LIST, LB_INSERTSTRING, update_device, (LPARAM) blue->BD_NAME);
+
+              if (update_device == -1)
+              {
+                bluetooth_configs[cur_device] = blue;
+                cur_device++;
+              } else
+              {
+                HWND hList = GetDlgItem (parent, ID_CONFIG_LIST);
+                SendMessage (hList, LB_DELETESTRING, (WPARAM) update_device + 1, 0);
+              }
+
+              EndDialog (hwnd, 0);
+            }
+          } 
+          else if (len_name == 0)
+            MessageBox (hwnd, "You didn't enter a name!", "Error", MB_OK);
+          else if (len_addr == 0)
+            MessageBox (hwnd, "You didn't enter and address!", "Error", MB_OK);
+          else
+            MessageBox (hwnd, "You didn't enter anything!", "Error", MB_OK);
+
+        }
+        break;
+        case ID_CREATE_FULL:
+        {
+          if (SendDlgItemMessage(hwnd, ID_CREATE_FULL, BM_GETCHECK, 0, 0) == 0)
+          {
+            SendDlgItemMessage(hwnd, ID_CREATE_FULL, BM_SETCHECK, 1, 0);
+            SendDlgItemMessage(hwnd, ID_CREATE_SINGLE, BM_SETCHECK, 0, 0);
+          }
+        }
+        break;
+        case ID_CREATE_SINGLE:
+        {
+          if (SendDlgItemMessage(hwnd, ID_CREATE_SINGLE, BM_GETCHECK, 0, 0) == 0)
+          {
+            SendDlgItemMessage(hwnd, ID_CREATE_SINGLE, BM_SETCHECK, 1, 0);
+            SendDlgItemMessage(hwnd, ID_CREATE_FULL, BM_SETCHECK, 0, 0);
+          }
+        }
+        break;
+      }
+    break;
+    case WM_CLOSE:
+      EndDialog (hwnd, 0);
+    break;
+    default:
+      return FALSE;
+  }
+  return FALSE;
+}
+
+BOOL CALLBACK
+ConfigEditDlgProc (HWND hwnd, UINT  msg, WPARAM wParam, LPARAM lParam)
+{
+  switch (msg)
+  {
+    case WM_INITDIALOG:
+    {
+      int i;
+      for (i = 0; i < cur_device; i++)
+        SendDlgItemMessage (hwnd, ID_CONFIG_LIST, LB_ADDSTRING, 0, (LPARAM) bluetooth_configs[i]->BD_NAME);
+    }
+    break;
+    case WM_COMMAND:
+      switch (LOWORD (wParam))
+      {
+        case ID_CONFIG_UPDATE:
+        {
+          HWND hList = GetDlgItem (hwnd, ID_CONFIG_LIST);
+          int count = SendMessage(hList, LB_GETSELCOUNT, 0, 0);
+          if (count != LB_ERR)
+          {
+            if (count != 0)
+            {
+              if (count == 1)
+              {
+                int *buf = (int*) GlobalAlloc (GPTR, sizeof (int) * count);
+                SendMessage (hList, LB_GETSELITEMS, (WPARAM) count, (LPARAM) buf);
+                update_device = buf[0];
+
+                int ret = DialogBox (GetModuleHandle (NULL),
+                  MAKEINTRESOURCE (ID_NOSLEEP_CREATE), hwnd, ConfigCreateDlgProc);
+
+                if (ret == -1)
+                  MessageBox (hwnd, "Dialog failed!", "Error",
+                    MB_OK | MB_ICONINFORMATION);
+
+                update_device = -1;
+                GlobalFree (buf);
+              } else
+                MessageBox (hwnd, "Select only one item for update.", "Error", MB_OK);
+            } else 
+              MessageBox (hwnd, "No items selected.", "Warning", MB_OK);
+          } else
+            MessageBox (hwnd, "Error counting items :(", "Error", MB_OK);
+        }
+        break;
+        case ID_CONFIG_CREATE:
+        {
+          if (cur_device < MAX_DEVICES)
+          {
+            int ret = DialogBox (GetModuleHandle (NULL),
+              MAKEINTRESOURCE (ID_NOSLEEP_CREATE), hwnd, ConfigCreateDlgProc);
+
+            if (ret == -1)
+              MessageBox (hwnd, "Dialog failed!", "Error",
+                MB_OK | MB_ICONINFORMATION);
+          } else
+            MessageBox (hwnd, "Too many stored addresses! Must delete some before adding more.", "Error", MB_OK);
+        }
+        break;
+        case ID_CONFIG_DELETE:
+        {
+          HWND hList = GetDlgItem (hwnd, ID_CONFIG_LIST);
+          int count = SendMessage(hList, LB_GETSELCOUNT, 0, 0);
+          if (count != LB_ERR)
+          {
+            if (count != 0)
+            {
+              int i, j;
+              int *buf = (int*) GlobalAlloc (GPTR, sizeof (int) * count);
+              SendMessage (hList, LB_GETSELITEMS, (WPARAM) count, (LPARAM) buf);
+
+              for (i = 0; i < count; i++)
+              {
+                SendMessage (hList, LB_DELETESTRING, (WPARAM) buf[i], 0);
+                GlobalFree ((HANDLE) bluetooth_configs[buf[i]]);
+                for (j = buf[i]; i < cur_device; i++)
+                  bluetooth_configs[j] = bluetooth_configs[j + 1];
+              }
+
+              GlobalFree (buf);
+              cur_device -= count;
+            } else 
+              MessageBox (hwnd, "No items selected.", "Warning", MB_OK);
+          } else
+            MessageBox (hwnd, "Error counting items :(", "Error", MB_OK);
+        }
+        break;
+        case ID_CONFIG_CLEAR:
+        {
+          SendDlgItemMessage (hwnd, ID_CONFIG_LIST, LB_RESETCONTENT, 0, 0);
+          int i;
+          for (i = 0; i < cur_device; i++)
+            GlobalFree ((HANDLE) bluetooth_configs[i]);
+          cur_device = 0;
+        }
+        break;
+        case ID_NOSLEEP_LIST:
+          switch (HIWORD (wParam))
+          {
+            case LBN_SELCHANGE:
+            {
+              HWND hList = GetDlgItem (hwnd, ID_CONFIG_LIST);
+              int count = SendMessage (hList, LB_GETSELCOUNT, 0, 0);
+              if (count != LB_ERR)
+              {
+                if (count == 1)
+                {
+                  int index;
+                  int err = SendMessage (hList, LB_GETSELITEMS, (WPARAM) 1, (LPARAM) &index);
+
+                  if (err != LB_ERR)
+                    SendMessage (hList, LB_GETITEMDATA, (WPARAM) index, 0);
+                  else
+                    MessageBox (hwnd, "Error getting selected item :(", "Error", MB_OK);
+                }
+              } else
+                MessageBox (hwnd, "Error counting items :(", "Error", MB_OK);
+            }
+            break;
+          }
+        break;
+      }
+    break;
+    case WM_CLOSE:
+      EndDialog (hwnd, 0);
+    break;
+    default:
+      return FALSE;
+  }
+  return TRUE;
+}
+
+BOOL CALLBACK
+AboutDlgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+  switch(msg)
+  {
+    case WM_INITDIALOG:
+      return TRUE;
+    case WM_COMMAND:
+      switch (LOWORD (wParam))
+      {
+        case IDOK:
+          EndDialog (hwnd, IDOK);
+        break;
+        case IDCANCEL:
+          EndDialog (hwnd, IDCANCEL);
+        break;
+      }
+    break;
+    default:
+      return FALSE;
+  }
+  return TRUE;
+}
+
+void
+DoHelpAbout (HWND hwnd)
+{
+  int ret = DialogBox (GetModuleHandle (NULL), 
+      MAKEINTRESOURCE (ID_NOSLEEP_ABOUT), hwnd, AboutDlgProc);
+  if (ret == -1)
+    MessageBox (hwnd, "About dialog failed!", "Error",
+      MB_OK | MB_ICONINFORMATION);
+}
+
+void
+DoFileCreate (HWND hwnd)
+{
+  ;
+}
+
+void
+DoFileOpen (HWND hwnd)
+{
+  if (cur_device != 0)
+  {
+    int iok = MessageBox (hwnd, "This will overwrite current configs! \n Would you like to proceed?", "Warning",
+      MB_OKCANCEL | MB_ICONWARNING);
+    if (iok != IDOK)
+      return;
+  }
+
   OPENFILENAME ofn;
   char szFileName[MAX_PATH] = "";
 
-  ZeroMemory(&ofn, sizeof(ofn));
+  ZeroMemory (&ofn, sizeof (ofn));
 
   ofn.lStructSize = sizeof(OPENFILENAME);
   ofn.hwndOwner = hwnd;
-  ofn.lpstrFilter = "Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
+  ofn.lpstrFilter = "Cube Config (*.ccfg)\0*.ccfg\0All Files (*.*)\0*.*\0";
   ofn.lpstrFile = szFileName;
   ofn.nMaxFile = MAX_PATH;
+  ofn.lpstrDefExt = "ccfg";
   ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
-  ofn.lpstrDefExt = "txt";
 
-  if(GetOpenFileName (&ofn))
+  if ( GetOpenFileName (&ofn))
   {
     HWND hEdit = GetDlgItem (hwnd, ID_NOSLEEP_MAIN);
-    // LoadTextFileToEdit (hEdit, szFileName);
+    LoadConfig (hEdit, szFileName);
   }
 }
 
-void DoFileSave (HWND hwnd)
+void
+DoFileSave (HWND hwnd)
 {
   OPENFILENAME ofn;
   char szFileName[MAX_PATH] = "";
 
-  ZeroMemory(&ofn, sizeof(ofn));
+  ZeroMemory (&ofn, sizeof (ofn));
 
   ofn.lStructSize = sizeof (OPENFILENAME);
   ofn.hwndOwner = hwnd;
-  ofn.lpstrFilter = "Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
+  ofn.lpstrFilter = "Cube Config (*.ccfg)\0*.ccfg\0All Files (*.*)\0*.*\0";
   ofn.lpstrFile = szFileName;
   ofn.nMaxFile = MAX_PATH;
-  ofn.lpstrDefExt = "txt";
+  ofn.lpstrDefExt = "ccfg";
   ofn.Flags = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT;
 
   if (GetSaveFileName (&ofn))
   {
     HWND hEdit = GetDlgItem (hwnd, ID_NOSLEEP_MAIN);
-    // SaveTextFileFromEdit (hEdit, szFileName);
+    SaveConfig (hEdit, szFileName);
   }
 }
 
-void DoHelpAbout (HWND hwnd)
+BOOL
+SaveConfig (HWND hEdit, LPCTSTR pszFileName)
 {
-  ;
+  HANDLE hFile;
+  BOOL bSuccess = FALSE;
+
+  hFile = CreateFile (pszFileName, GENERIC_WRITE, 0, NULL,
+    CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+  if (hFile != INVALID_HANDLE_VALUE)
+  {
+    DWORD dwTextLength;
+
+    dwTextLength = GetWindowTextLength (hEdit);
+
+    if (0 < dwTextLength)
+    {
+      LPSTR pszText;
+      DWORD dwBufferSize = dwTextLength + 1;
+
+      pszText = (LPSTR) GlobalAlloc (GPTR, dwBufferSize);
+      if(pszText != NULL)
+      {
+        if (GetWindowText (hEdit, pszText, dwBufferSize))
+        {
+          DWORD dwWritten;
+
+          if (WriteFile (hFile, pszText, dwTextLength, &dwWritten, NULL))
+            bSuccess = TRUE;
+        }
+        GlobalFree (pszText);
+      }
+    }
+    CloseHandle (hFile);
+  }
+  return bSuccess;
+}
+
+BOOL
+LoadConfig (HWND hEdit, LPCTSTR pszFileName)
+{
+  HANDLE hFile;
+  BOOL bSuccess = FALSE;
+
+  hFile = CreateFile (pszFileName, GENERIC_READ, FILE_SHARE_READ, NULL,
+    OPEN_EXISTING, 0, NULL);
+  if (hFile != INVALID_HANDLE_VALUE)
+  {
+    DWORD dwFileSize;
+
+    dwFileSize = GetFileSize (hFile, NULL);
+    if (dwFileSize != 0xFFFFFFFF)
+    {
+      LPSTR pszFileText;
+
+      pszFileText = (LPSTR) GlobalAlloc (GPTR, dwFileSize + 1);
+      if (pszFileText != NULL)
+      {
+        DWORD dwRead;
+
+        if (ReadFile (hFile, pszFileText, dwFileSize, &dwRead, NULL))
+        {
+          pszFileText[dwFileSize] = 0;
+          if (SetWindowText (hEdit, pszFileText))
+            bSuccess = TRUE;
+        }
+        GlobalFree (pszFileText);
+      }
+    }
+    CloseHandle (hFile);
+  }
+  return bSuccess;
 }
--- GUI/resource.h
+++ GUI/resource.h
@@ -6,18 +6,34 @@
 #define ID_NOSLEEP_MENU                 101
 #define ID_NOSLEEP_ICON                 102
 #define ID_NOSLEEP_MAIN                 103
+#define ID_NOSLEEP_ABOUT                104
+#define ID_NOSLEEP_LIST                 105
+#define ID_NOSLEEP_CREATE               106
 
 #define ID_HELP_ABOUT                   901
 #define ID_FILE_EXIT                    902
-#define ID_FILE_OPEN                    903
-#define ID_FILE_SAVEAS                  904
-#define ID_FILE_NEW                     905
+#define ID_CONFIG_EDIT                  903
+#define ID_CONFIG_UPDATE                904
+#define ID_CONFIG_CREATE                905
+#define ID_CONFIG_SAVEAS                906
+#define ID_CONFIG_LOAD                  907
+#define ID_CONFIG_DELETE                908
+#define ID_CONFIG_CLEAR                 909
+#define ID_CONFIG_CONNECT               910
+#define ID_CONFIG_TEXT                  911
+#define ID_CONFIG_LIST                  912
+#define ID_CREATE_NAME                  913
+#define ID_CREATE_ADDR                  914
+#define ID_CREATE_FULL                  915
+#define ID_CREATE_SINGLE                916
+#define ID_CREATE_ADD                   917
+
 
 /* Next default values for new objects. */
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
-#define _APS_NEXT_COMMAND_VALUE         906
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         918
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
 #endif

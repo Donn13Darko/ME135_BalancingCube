f4813460ad9b1a5fb477e38056355f6a26d01794
Mitchell Oleson <moleson21@gmail.com>
2017-04-25 13:38:53 -0700

    Removed struct

--- GUI/main.c
+++ GUI/main.c
@@ -26,8 +26,8 @@ struct blue_config *bluetooth_configs[MAX_DEVICES];
 int cur_device = 0;
 int update_device = -1;
 int connected_device = -1;
-struct SOCKET blue_sock;
-struct SOCKADDR_BTH blue_conn;
+SOCKET blue_sock;
+SOCKADDR_BTH blue_conn;
 
 /* Function Prototypes. */
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
